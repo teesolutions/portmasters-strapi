ccf8a07a84a7245effa216f374722e424eb44066
Iman Rabbi <teesolutionstechteam@gmail.com>
2026-06-02 10:53:05 +0330

    feat: add blog entities

--- src/index.js
+++ src/index.js
@@ -16,5 +16,50 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  async bootstrap({ strapi }) {
+    const role = await strapi.db
+      .query('plugin::users-permissions.role')
+      .findOne({ where: { type: 'public' } });
+
+    if (!role) {
+      return;
+    }
+
+    const actionsToEnable = [
+      'api::post.post.find',
+      'api::post.post.findOne',
+      'api::category.category.find',
+      'api::category.category.findOne',
+      'api::tag.tag.find',
+      'api::tag.tag.findOne',
+    ];
+
+    for (const action of actionsToEnable) {
+      const existingPermission = await strapi.db
+        .query('plugin::users-permissions.permission')
+        .findOne({
+          where: {
+            action,
+            role: role.id,
+          },
+        });
+
+      if (existingPermission) {
+        if (!existingPermission.enabled) {
+          await strapi.db.query('plugin::users-permissions.permission').update({
+            where: { id: existingPermission.id },
+            data: { enabled: true },
+          });
+        }
+      } else {
+        await strapi.db.query('plugin::users-permissions.permission').create({
+          data: {
+            action,
+            role: role.id,
+            enabled: true,
+          },
+        });
+      }
+    }
+  },
 };
